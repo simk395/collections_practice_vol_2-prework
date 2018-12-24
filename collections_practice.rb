28928a2c713ca73f61f756254c28f7f018e2d92e
Simon Kong <simon.kong95@gmail.com>
2018-12-24 19:52:42 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,56 @@
 # your code goes here
+require "pry"
+
+def begins_with_r(array)
+  array.each do |item|
+    if item[0] != "r"
+      return false
+    end
+  end
+  return true
+end
+
+def contain_a(array)
+  i = []
+  array.each do |item|
+    if item.include?("a")
+      i << item
+    end
+  end
+  i
+end
+
+def first_wa(array)
+  array.each do |item|
+    if item[0] == "w" && item[1] == "a"
+      return item
+    end
+  end
+end
+
+def remove_non_strings(array)
+  array.delete_if do |item|
+    item.class != String
+  end
+  array
+end
+
+def count_elements(array)
+  i, a = 0, []
+  array.each do |item|
+    if a.include?(item) == false
+      a << item
+      a[-1][:count] = 0
+    else
+      a[-1][:count] += 1
+  end
+  a
+end
+
+def find_cool(array)
+  array.each do |item|
+    if item.has_value?("cool")
+      return [item]
+    end
+  end
+end
